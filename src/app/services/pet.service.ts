361eca8d9c43f10f4f7a84c634ab7e1db1b030b2
Kat Ferreira <kat.ferreira@duocuc.cl>
2025-06-09 22:38:36 -0400

    add pet service

--- src/app/services/pet.service.ts
+++ src/app/services/pet.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Pet, Vaccine } from '../models/pet';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PetService {
+  private pets: Pet[] = [];
+  private nextId = 1;
 
-  constructor() { }
+  getAll(): Pet[] {return this.pets;}
+  
+  getById(id: number) { return this.pets.find(p => p.id === id); }
+
+  add(p: Omit<Pet,'id'|'vaccines'>) {
+    this.pets.push({ ...p, id: this.nextId++, vaccines: [] });
+  }
+  addVaccine(petId: number, v: Omit<Vaccine,'id'>) {
+    const pet = this.getById(petId);
+    if (pet) pet.vaccines.push({ ...v, id: pet.vaccines.length+1 });
+  }
 }
